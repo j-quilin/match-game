ec291be6661330e656f53da4979536b204c9bb5e
jackieb500 <jacquelline500@gmail.com>
2018-10-08 20:03:29 -0700

    Add Routes and Configure router in app component

---
+++ routes/game.js
@@ -0,0 +1,10 @@
+// route/game
+
+const Game = () => (
+    <div>
+        <h1>Game</h1>
+        <p>This is the Game route.</p>
+    </div>
+);
+
+export default Game;
---
+++ routes/home.js
@@ -0,0 +1,10 @@
+    // routes/home
+
+    const Home = () => (
+        <div>
+            <h1>Home</h1>
+            <p>This is the Home route.</p>
+        </div>
+    );
+
+    export default Home;
